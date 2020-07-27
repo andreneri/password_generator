f5f6ca51689bc97fe22d2be564c7b7aa9595b73f
André <andreneri22@gmail.com>
2020-07-27 13:20:56 +0100

    changed home.html to index.html and added bootstrap to about.html

--- generator/views.py
+++ generator/views.py
@@ -5,7 +5,7 @@ import random
 # Create your views here.
 
 def home(request):
-    return render(request, 'generator/home.html')
+    return render(request, 'generator/index.html')
 
 
 def generate(request):
